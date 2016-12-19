1465bf637bbfd9c79a9758527a6434d51aeafe54
bvalyan <bvalyan@hotmail.com>
2016-12-19 15:28:05 -0500

    Implemented home screen.

--- app/src/main/java/com/optimalotaku/paraguide/HeroPull.java
+++ app/src/main/java/com/optimalotaku/paraguide/HeroPull.java
@@ -1,60 +1,21 @@
 package com.optimalotaku.paraguide;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+/**
+ * Created by Brandon on 12/19/16.
+ */
 
-public class MainActivity  extends AppCompatActivity implements AsyncResponse {
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.homescreen);
-
-    }
-
-    public void heroClick(View view){
-        setContentView(R.layout.activity_main);
-        //temporary workaround. Needs to be pulled out.
-    }
-
-    public void onClick(View view) throws InterruptedException {
-
-        //Gather UI Objects
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        EditText hEdit = (EditText) findViewById(R.id.heroText);
-
-        //Create HeroData Object
-        HeroData heroData = new HeroData();
-
-        ParagonAPIHelper paraHelper = new ParagonAPIHelper(progressBar,hEdit.getText().toString(),heroData);
-        paraHelper.delegate = this;
-        paraHelper.execute();
-    }
-
-    public void loginSend(View view){
-        View view2 = this.getCurrentFocus();
-        if(view2 != null){
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view2.getWindowToken(), 0);
-        }
-        Intent intent = new Intent(MainActivity.this, DeckView.class);
-        startActivity(intent);
-    }
-
-    @Override
+public class HeroPull extends AppCompatActivity {
     public void processFinish(HeroData hData){
         ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
         TextView responseView = (TextView) findViewById(R.id.responseView);
@@ -69,32 +30,32 @@ public class MainActivity  extends AppCompatActivity implements AsyncResponse {
 
         //Set Summary Text
         //Mobility
-        if(isBetween(hData.getMobility(),1,3)){
+        if(MainActivity.isBetween(hData.getMobility(),1,3)){
             responseView.setText(Constants.LOW_MOBILITY_STATEMENT);
             responseView.append("\n");
         }
-        else if(isBetween(hData.getMobility(),4,7)){
+        else if(MainActivity.isBetween(hData.getMobility(),4,7)){
             responseView.setText(Constants.MED_MOBILITY_STATEMENT);
             responseView.append("\n");
         }
-        else if(isBetween(hData.getMobility(),8,10)){
+        else if(MainActivity.isBetween(hData.getMobility(),8,10)){
             responseView.setText(Constants.HI_MOBILITY_STATEMENT);
             responseView.append("\n");
         }
         else{
-           Log.i("INFO","No valid Mobility given: "+hData.getMobility().toString());
+            Log.i("INFO","No valid Mobility given: "+hData.getMobility().toString());
         }
 
         //Basic Attack
-        if(isBetween(hData.getBasicAttack(),1,3)){
+        if(MainActivity.isBetween(hData.getBasicAttack(),1,3)){
             responseView.append(Constants.LOW_BASIC_ATTACK_STATEMENT);
             responseView.append("\n");
         }
-        else if(isBetween(hData.getBasicAttack(),4,7)){
+        else if(MainActivity.isBetween(hData.getBasicAttack(),4,7)){
             responseView.append(Constants.MED_BASIC_ATTACK_STATEMENT);
             responseView.append("\n");
         }
-        else if(isBetween(hData.getBasicAttack(),8,10)){
+        else if(MainActivity.isBetween(hData.getBasicAttack(),8,10)){
             responseView.append(Constants.HI_BASIC_ATTACK_STATEMENT);
             responseView.append("\n");
         }
@@ -103,15 +64,15 @@ public class MainActivity  extends AppCompatActivity implements AsyncResponse {
         }
 
         //Durability
-        if(isBetween(hData.getDurability(),1,3)){
+        if(MainActivity.isBetween(hData.getDurability(),1,3)){
             responseView.append(Constants.LOW_DURABILITY_STATEMENT);
             responseView.append("\n");
         }
-        else if(isBetween(hData.getDurability(),4,7)){
+        else if(MainActivity.isBetween(hData.getDurability(),4,7)){
             responseView.append(Constants.MED_DURABILITY_STATEMENT);
             responseView.append("\n");
         }
-        else if(isBetween(hData.getDurability(),8,10)){
+        else if(MainActivity.isBetween(hData.getDurability(),8,10)){
             responseView.append(Constants.HI_DURABILITY_STATEMENT);
             responseView.append("\n");
         }
@@ -121,15 +82,15 @@ public class MainActivity  extends AppCompatActivity implements AsyncResponse {
 
 
         //Ability Attack
-        if(isBetween(hData.getAbilityAttack(),1,3)){
+        if(MainActivity.isBetween(hData.getAbilityAttack(),1,3)){
             responseView.append(Constants.LOW_ABILITY_ATTACK_STATEMENT);
             responseView.append("\n");
         }
-        else if(isBetween(hData.getAbilityAttack(),4,7)){
+        else if(MainActivity.isBetween(hData.getAbilityAttack(),4,7)){
             responseView.append(Constants.MED_ABILITY_ATTACK_STATEMENT);
             responseView.append("\n");
         }
-        else if(isBetween(hData.getAbilityAttack(),8,10)){
+        else if(MainActivity.isBetween(hData.getAbilityAttack(),8,10)){
             responseView.append(Constants.HI_ABILITY_ATTACK_STATEMENT);
             responseView.append("\n");
         }
@@ -146,11 +107,4 @@ public class MainActivity  extends AppCompatActivity implements AsyncResponse {
         }
 
     }
-
-    //Helper Functions
-    public static boolean isBetween( Integer x, Integer lower, Integer upper) {
-        return lower <= x && x <= upper;
-    }
-
 }
-
