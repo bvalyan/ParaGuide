bb748d4514c9b4d2d0bcda0b163ed20fce669a3c
jerekwilson <jerekwilson@gmail.com>
2017-01-05 17:45:21 -0500

    updating attribute replace

--- app/src/main/java/com/optimalotaku/paraguide/ParagonAPIAttrReplace.java
+++ app/src/main/java/com/optimalotaku/paraguide/ParagonAPIAttrReplace.java
@@ -6,7 +6,23 @@ package com.optimalotaku.paraguide;
 
 public class ParagonAPIAttrReplace {
 
-    public static final String manaAttr = "{attr:mp}";
+    public static final String mpAttr = "{attr:mp}";
+    public static final String hpAttr = "{attr:hp}";
+    public static final String lfstlAttr = "{attr:lfstl}";
+    public static final String mpRegAttr = "{attr:mpreg}";
+    public static final String hpRegAttr = "{attr:hpreg}";
+    public static final String physDmgAttr = "{attr:physdmg}";
+    public static final String dmgBnsAttr = "{attr:dmgbns}";
+    public static final String enarAttr = "{attr:enar}";
+    public static final String spdAttr = "{attr:spd}";
+    public static final String endmgAttr = "{attr:endmg}";
+    public static final String shldAttr = "{attr:shld}";
+    public static final String physArAttr = "{attr:physar}";
+    public static final String atkSpdAttr = "{attr:atkspd}";
+    public static final String cdrAttr = "{attr:cdr}";
+    public static final String dmgAttr = "{attr:dmg}";
+    public static final String crtChAttr = "{attr:crtch}";
 
 
+    
 }
