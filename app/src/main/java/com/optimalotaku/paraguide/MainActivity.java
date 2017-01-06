afebb92c5ce7ce17a88d26375c8de8ed5adc3f87
bvalyan <bvalyan@hotmail.com>
2017-01-06 11:52:08 -0500

    Small formatting fixes removed bullets from cotd added cotd text

--- app/src/main/java/com/optimalotaku/paraguide/MainActivity.java
+++ app/src/main/java/com/optimalotaku/paraguide/MainActivity.java
@@ -89,33 +89,33 @@ public class MainActivity extends AppCompatActivity implements CardInfoResponse 
 
         cotdText.setText("Name: "+cardOfTheDay.getName()+"\n\n");
 
-        cotdText.append("Card Effects:\n");
+        cotdText.append("Card Effects:\n\n");
         List<CardEffect> effectList = cardOfTheDay.getEffectList();
         for(CardEffect eff: effectList) {
             if(eff.getStat() != null && eff.getStatValue() != null) {
                 Log.i("INFO","MainActivity - processCardInfoFinish - Stat: "+eff.getStat()+" Human Readable: "+ cardOfTheDay.statToHumanReadable(eff.getStat()));
-                cotdText.append("\t• " + cardOfTheDay.statToHumanReadable(eff.getStat()) +": "+eff.getStatValue()+"\n");
+                cotdText.append("" + cardOfTheDay.statToHumanReadable(eff.getStat()) +": "+eff.getStatValue()+"\n");
             }
             if(eff.getDescription() != null){
-               cotdText.append("\t• " + attrTranslator.replaceSymbolsWithText(eff.getDescription()) + "\n");
+               cotdText.append("" + attrTranslator.replaceSymbolsWithText(eff.getDescription()) + "\n");
             }
             if(eff.getCooldown()!= null && Integer.parseInt(eff.getCooldown()) > 1){
-                cotdText.append("\t• Cooldown: " + eff.getCooldown() + "s\n" );
+                cotdText.append(" Cooldown: " + eff.getCooldown() + "s\n" );
             }
         }
         cotdText.append("\n");
 
-        cotdText.append("Max Card Effects:\n");
+        cotdText.append("Fully Upgraded Card Effects:\n");
         List<CardEffect> maxEffectList = cardOfTheDay.getMaxEffectList();
         for(CardEffect eff: maxEffectList) {
             if(eff.getStat() != null && eff.getStatValue() != null) {
-                cotdText.append("\t• " + cardOfTheDay.statToHumanReadable(eff.getStat()) +": "+eff.getStatValue()+"\n");
+                cotdText.append("• " + cardOfTheDay.statToHumanReadable(eff.getStat()) +": "+eff.getStatValue()+"\n");
             }
             if(eff.getDescription() != null){
-                cotdText.append("\t• " + attrTranslator.replaceSymbolsWithText(eff.getDescription()) + "\n");
+                cotdText.append("• " + attrTranslator.replaceSymbolsWithText(eff.getDescription()) + "\n");
             }
             if(eff.getCooldown()!= null && Integer.parseInt(eff.getCooldown()) > 1){
-                cotdText.append("\t• Cooldown: " + eff.getCooldown() + "s\n" );
+                cotdText.append("• Cooldown: " + eff.getCooldown() + "s\n" );
             }
         }
 
