5f2fb70eea245890f0e56768ce0274d7d40562a8
bvalyan <bvalyan@hotmail.com>
2017-01-07 15:18:28 -0500

    Cleaned up code

--- app/src/main/java/com/optimalotaku/paraguide/MainActivity.java
+++ app/src/main/java/com/optimalotaku/paraguide/MainActivity.java
@@ -1,13 +1,11 @@
 package com.optimalotaku.paraguide;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -62,27 +60,6 @@ public class MainActivity extends AppCompatActivity implements CardInfoResponse 
         });
     }
 
-    public void heroClick(View view){
-        View view2 = this.getCurrentFocus();
-        if(view2 != null){
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view2.getWindowToken(), 0);
-        }
-
-        Intent intent = new Intent(MainActivity.this, HeroView.class);
-        startActivity(intent);
-    }
-
-
-    public void loginSend(View view){
-        View view2 = this.getCurrentFocus();
-        if(view2 != null){
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view2.getWindowToken(), 0);
-        }
-        Intent intent = new Intent(MainActivity.this, DeckView.class);
-        startActivity(intent);
-    }
 
     public void getCardData(){
         ParagonAPICardInfo cardInfo = new ParagonAPICardInfo();
