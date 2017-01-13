2368cd4534b16daa22bfdf39b1a438c11faf31c5
jerekwilson <jerekwilson@gmail.com>
2017-01-13 09:25:00 -0500

    updating shield stat attribute to be singular

--- app/src/main/java/com/optimalotaku/paraguide/ParagonAPIAttrReplace.java
+++ app/src/main/java/com/optimalotaku/paraguide/ParagonAPIAttrReplace.java
@@ -12,7 +12,7 @@ public class ParagonAPIAttrReplace {
                                    ,"{attr:spd}","{attr:endmg}","{attr:shld}","{attr:physar}","{attr:atkspd}","{attr:cdr}","{attr:dmg}","{attr:crtch}"};
 
     private String[] attrTextList = {"MP","HP","LifeSteal","MP Regen","HP Regen","Power","Damage Bonus","Ability Armor"
-                                   ,"Max Movement Speed","Power","Shields","Basic Armor","Attack Speed","Cooldown Reduction","Power","Critical Chance"};
+                                   ,"Max Movement Speed","Power","Shield","Basic Armor","Attack Speed","Cooldown Reduction","Power","Critical Chance"};
 
     private String[] statSymbList = {"{status:burn}","{status:slow}","{status:psn}","{status:bleed}"};
     private String[] statTextList = {"Burn","Slow","Poison","Bleed"};
