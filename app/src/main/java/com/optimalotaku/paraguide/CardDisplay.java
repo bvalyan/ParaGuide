e5174b8c6c66f813d083d1e134716a68251ca8ef
bvalyan <brandon.valyan@coxinc.com>
2017-08-03 17:22:17 -0400

    New Card display classes

--- app/src/main/java/com/optimalotaku/paraguide/CardDisplay.java
+++ app/src/main/java/com/optimalotaku/paraguide/CardDisplay.java
@@ -4,7 +4,6 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
-import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
@@ -12,8 +11,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-
-import java.util.List;
 
 /**
  * Created by bvaly on 1/29/2017.
@@ -47,9 +44,12 @@ public class CardDisplay extends AppCompatActivity{
         TextView cotdTitle   = (TextView) findViewById(R.id.cotdTitle);
         TextView cotdEff = (TextView) findViewById(R.id.cotdeff);
         TextView cotdCD = (TextView) findViewById(R.id.cotd_cd);
-        TextView cotdFullEff = (TextView) findViewById(R.id.cotd_full_eff);
-        TextView effecttitle = (TextView) findViewById(R.id.efftitle);
         TextView cdTitle = (TextView) findViewById(R.id.cdtitle);
+        TextView vitCost = (TextView) findViewById(R.id.vitality_gem_cost);
+        TextView dexCost = (TextView) findViewById(R.id.dexterity_gem_cost);
+        TextView intCost = (TextView) findViewById(R.id.intellect_gem_cost);
+        TextView goldCost = (TextView) findViewById(R.id.gold_cost);
+        TextView rarity = (TextView) findViewById(R.id.rarity);
         String cotdTitle1;
         String cotdEff2 = new String();
         String cotdCooldown = new String();
@@ -63,51 +63,28 @@ public class CardDisplay extends AppCompatActivity{
         cotdTitle1 = cotd.getName();
 
 
-        List<CardEffect> effectList = cotd.getEffectList();
-        for(CardEffect eff: effectList) {
-            if(eff.getStat() != null && eff.getStatValue() != null) {
-                Log.i("INFO","MainActivity - processCardInfoFinish - Stat: "+eff.getStat()+" Human Readable: "+ cotd.statToHumanReadable(eff.getStat()));
-                cotdEff2 = cotdEff2 + "• " + cotd.statToHumanReadable(eff.getStat()) +": "+eff.getStatValue()+"\n";
-            }
-            if(eff.getDescription() != null){
-                String apiText = attrTranslator.replaceStatWithText(eff.getDescription());
-                cotdEff2 = cotdEff2 + "• "+apiText+"\n";
-            }
-            if(eff.getCooldown()!= null && Integer.parseInt(eff.getCooldown()) > 1){
-                cdTitle.setVisibility(View.VISIBLE);
-                cotdCooldown =  eff.getCooldown();
-            }
-        }
+        int level= 0;
+        String effectName = cotd.getCardLevels().get(level).getAbilites().get(0).getName();
+        String description = cotd.getCardLevels().get(level).getAbilites().get(0).getDescription();
+        int vitality = cotd.getVitalityGemCost();
+        int dexterity = cotd.getDexterityGemCost();
+        int intellect = cotd.getIntellectGemCost();
+        int gold = cotd.getGoldCost();
+        String rarityText = cotd.getRarity();
 
 
-        List<CardEffect> maxEffectList = cotd.getMaxEffectList();
-        if(maxEffectList.size() > 0) {
-            cotdFullEff.setVisibility(View.VISIBLE);
-            effecttitle.setVisibility(View.VISIBLE);
-            for (CardEffect eff : maxEffectList) {
-                if (eff.getStat() != null && eff.getStatValue() != null) {
-                    cotdFullEffect =   "• " + cotd.statToHumanReadable(eff.getStat()) + ": " + eff.getStatValue() + "\n";
-                }
-                if (eff.getDescription() != null) {
-                    String apiText = attrTranslator.replaceStatWithText(eff.getDescription());
-                    cotdFullEffect = cotdFullEffect + "• "+apiText+"\n";
-                }
-                if (eff.getCooldown() != null && Integer.parseInt(eff.getCooldown()) > 1) {
-                    cotdFullEffect = cotdFullEffect + "• Cooldown: " + eff.getCooldown() + "s\n";
-                }
-            }
-        }
 
         SpannableString ss = attrTranslator.replaceSymbolsWithImages(this,cotdFullEffect);
-        SpannableString ss2 = attrTranslator.replaceSymbolsWithImages(this,cotdEff2);
+        SpannableString ss2 = attrTranslator.replaceSymbolsWithImages(this,description);
+
         cotdTitle.setText(cotdTitle1);
         cotdEff.setText(ss2);
         cotdCD.setText(cotdCooldown);
-        cotdFullEff.setText(ss);
-
-
-
-
+        vitCost.setText(vitality);
+        dexCost.setText(dexterity);
+        intCost.setText(intellect);
+        rarity.setText(rarityText);
+        goldCost.setText(gold);
     }
 
 }
