d9c00b11c331de4373f396dad3cf949f4204a4f0
bvalyan <bvalyan@hotmail.com>
2018-04-17 15:58:31 -0400

    Adjusted header transparency

--- app/src/main/java/com/optimalotaku/paraguide/MainActivity.java
+++ app/src/main/java/com/optimalotaku/paraguide/MainActivity.java
@@ -129,6 +129,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
+        drawer.setScrimColor(Color.TRANSPARENT);
         toggle.syncState();
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
